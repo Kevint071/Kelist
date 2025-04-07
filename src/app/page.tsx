91eaee9ff415925e53d53a007e0df7695cd16d0e
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-04-07 16:37:39 -0500

    feat: add metadata for page title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import FeaturesSection from "@/components/Home/FeaturesSection/FeaturesSection";
-import BackgroundAnimation from "@/components/BackGroundAnimation";
 import MainSection from "@/components/Home/MainSection";
 import CTASection from "@/components/Home/CTASection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kelist",
+  description: "Tus tareas en orden, tus metas en acción",
+};
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen overflow-hidden bg-gray-950 text-gray-100">
-      <BackgroundAnimation />
+    <div className="relative min-h-screen overflow-hidden text-gray-100">
       <div className="relative z-10 container mx-auto px-6 pt-24 pb-12">
         <MainSection />
         <FeaturesSection />
