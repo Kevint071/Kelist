3440a08d99d2d665ad6e55454af2fed11af40ee8
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-04-07 16:57:43 -0500

    refactor: improve styling and structure of CTASection and simplify ButtonMouse component

--- src/components/Home/CTASection.tsx
+++ src/components/Home/CTASection.tsx
@@ -2,11 +2,11 @@ import ButtonMouse from '@/components/ui/ButtonMouse'
 
 function CTASection() {
   return (
-    <section className="my-14 flex flex-col text-center">
-      <h2 className="mb-4 text-2xl font-bold text-slate-300 sm:text-3xl">
+    <section className="my-14 flex flex-col text-center px-6">
+      <h2 className="mb-4 max-lg:text-2xl font-bold text-slate-200 text-3xl">
         ¿Listo para aumentar tu productividad?
       </h2>
-      <p className="mb-8 text-lg text-gray-300 sm:text-xl">
+      <p className="mb-8 max-lg:text-lg text-gray-300 text-xl">
         Únete a miles de usuarios que ya están organizando sus vidas con nuestra
         app.
       </p>
